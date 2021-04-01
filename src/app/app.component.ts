e394ac4649b9eb3c7c71807979dd6e4492ebad13
pellax <tuxisalive@gmail.com>
2021-04-01 18:18:33 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "app.component.html"
+    selector: "gr-login",
+    moduleId: module.id,
+    templateUrl: "./login/login.component.html",
+    styleUrls:["./login/login.component.css"]
 })
 export class AppComponent { }
